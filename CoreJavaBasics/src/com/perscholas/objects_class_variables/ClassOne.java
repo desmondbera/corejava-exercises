c10c8e4d79ef330f7213171f35a1247c82f371ff
Desmond Beramendi <desmond.bera@gmail.com>
2019-06-03 18:21:05 -0400

    Variable App and 1st Class

---
+++ CoreJavaBasics/src/com/perscholas/objects_class_variables/ClassOne.java
@@ -0,0 +1,24 @@
+package com.perscholas.objects_class_variables;
+
+public class ClassOne {
+	String string1;
+	public String string2;
+	protected String string3;
+	private String string4;
+	
+	public ClassOne() {
+		this.string1 = "String one";
+		this.string2 = "String two";
+		this.string3 = "String three";
+		this.string4 = "String four";
+	}
+	
+	public void setString() {
+		
+	}
+	
+	public String getString4() {
+		return string4;
+	}
+	
+}
---
+++ CoreJavaBasics/src/com/perscholas/objects_class_variables/VariablesApp.java
@@ -0,0 +1,13 @@
+package com.perscholas.objects_class_variables;
+
+public class VariablesApp {
+
+	public static void main(String[] args) {
+		ClassOne first = new ClassOne();
+		System.out.println("1st str is: " + first.string1);
+		System.out.println("2nd str is: " + first.string2);
+		System.out.println("3rd str is: " + first.string3);
+		System.out.println("4th str is: " + first.getString4());
+	}
+
+}
